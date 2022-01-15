a23aebd010a2bac83f63e07f093361e5633cbacb
mitsuru_yamaura <komayou326@gmail.com>
2022-01-16 08:48:23 +0900

    GridButton.cs と GameManager.cs のリファクタリング。MVPパターンでの実装

---
+++ Assets/References.cs
@@ -0,0 +1,13 @@
+public class References
+{
+    // 参考　三目並べ(多次元配列)
+    // https://yuzame-gohan.com/marubatsu/
+
+    // 五目並べ(多次元配列)
+    // https://xr-hub.com/archives/13899
+
+    // MVP + UniRX
+    // https://takap-tech.com/entry/2021/11/27/195909
+
+    // https://virtualcast.jp/blog/2019/11/mvp_pattern_on_unity/
+}
